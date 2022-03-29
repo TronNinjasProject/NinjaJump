80becc629d67b56c33db4ee52cf78414eea291cf
mostaruk <moelle.mk@gmail.com>
2022-03-29 14:58:38 +0100

    Add files via upload

---
+++ scripts/project/scriptsInEvents.js
@@ -0,0 +1,40 @@
+"use strict";
+
+var Get_Score = "";//Taking the score to this variable.
+
+//Call this function when game end.
+function EndGame() {
+	
+		alert("My Score Is " + Get_Score);
+}
+
+
+{
+	const scriptsInEvents = {
+
+		async Game_Event55_Act17(runtime, localVars)
+		{
+			Get_Score = runtime.globalVars.score;
+		},
+
+		async Game_Event55_Act19(runtime, localVars)
+		{
+			EndGame();
+			
+		},
+
+		async Game_Event56_Act17(runtime, localVars)
+		{
+			Get_Score = runtime.globalVars.score;
+		},
+
+		async Game_Event56_Act19(runtime, localVars)
+		{
+			EndGame();
+			
+		}
+
+	};
+	
+	self.C3.ScriptsInEvents = scriptsInEvents;
+}
